23efece1537cffc9cad3253517fe5dd03582d7ad
Matteo Marchelli <matteo.marchelli@gmail.com>
2023-08-30 14:55:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# jtheo's dotfiles
+
+I'm managing my dotfiles with [chezmoi](https://github.com/twpayne/chezmoi)
+
+There's a lot of old stuff that I should clean, so it might be that a bash function isn't working, sorry...
+
+It's a working in progress
---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require 'wezterm'
+return {
+  initial_rows = 60,
+  initial_cols = 240,
+  font = wezterm.font 'JetBrains Mono',
+  -- color_scheme = 'Dark Pastel',
+  color_scheme = "Catppuccin Mocha", --  " Mocha", -- or Macchiato, Frappe, Latt
+  font_size = 14.0,
+
+
+  keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  },
+    window_frame = {
+    border_left_width = '0.2cell',
+    border_right_width = '0.2cell',
+    border_bottom_height = '0.2cell',
+    -- border_top_height = '0.25cell',
+    border_left_color = '#2a2f3b',
+    border_right_color = '#2a2f3b',
+    border_bottom_color = '#2a2f3b',
+    -- border_top_color = 'lightGray',
+  }
+}
+
