8f7b70d89270e12cbff74256a8abb0671e9c3038
Matteo Marchelli <matteo.marchelli@gmail.com>
2023-08-31 17:23:22 +0100

    more cleanup

--- private_dot_config/nvim/lua/plugins/go.lua
+++ private_dot_config/nvim/lua/plugins/go.lua
@@ -1,21 +1,6 @@
 -- adapted from https://github.com/amaanq/nvim-config/blob/master/lua/plugins/extras/lang/go.lua
 -- adapted from https://github.com/ueaner/nvimrc/blob/main/lua/plugins/extras/lang/example.lua
-return  --[[
-{
-  "ray-x/go.nvim",
-  dependencies = { -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    require("go").setup()
-  end,
-  event = { "CmdlineEnter" },
-  ft = { "go", "gomod" },
-  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-},
---]]{
+return {
   "ray-x/go.nvim",
   dependencies = {
     "ray-x/guihua.lua",
@@ -144,35 +129,6 @@ return  --[[
       end,
     },
   },
-},
---[[
-{
-  "jay-babu/mason-null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "williamboman/mason.nvim",
-    "jose-elias-alvarez/null-ls.nvim",
-  },
-  opts = function(_, opts)
-    local nls = require("null-ls")
-    vim.list_extend(opts.sources, {
-      nls.builtins.formatting.goimports,
-    })
-  end,
-}
-]]
---
--- setup formatters & linters
-{
-  "jose-elias-alvarez/null-ls.nvim",
-  opts = function(_, opts)
-    local nls = require("null-ls")
-    vim.list_extend(opts.sources, {
-      nls.builtins.formatting.gofumpt,
-      --nls.builtins.formatting.goimports,
-      nls.builtins.diagnostics.golangci_lint,
-    })
-  end,
 },
 {
   "nvim-neotest/neotest",
--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,20 +1,15 @@
 local wezterm = require 'wezterm'
+
 return {
   initial_rows = 60,
   initial_cols = 240,
+  hide_tab_bar_if_only_one_tab = true,
   font = wezterm.font 'JetBrains Mono',
   -- color_scheme = 'Dark Pastel',
   color_scheme = "Catppuccin Mocha", --  " Mocha", -- or Macchiato, Frappe, Latt
   font_size = 14.0,
 
-
-  keys = {
-    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
-    -- Make Option-Right equivalent to Alt-f; forward-word
-    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
-  },
-    window_frame = {
+  window_frame = {
     border_left_width = '0.2cell',
     border_right_width = '0.2cell',
     border_bottom_height = '0.2cell',
@@ -23,6 +18,13 @@ return {
     border_right_color = '#2a2f3b',
     border_bottom_color = '#2a2f3b',
     -- border_top_color = 'lightGray',
+  },
+
+  keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
   }
 }
 
