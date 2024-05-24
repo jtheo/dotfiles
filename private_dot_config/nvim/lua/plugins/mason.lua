1abbed3a7b0cdf23787f993fd2238adbfa36ee72
Matteo Marchelli <matteo.marchelli@gmail.com>
2024-05-24 16:31:48 +0100

    testing and messing nvim (need to rewrite from scratch...)

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -1,27 +1,27 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"awk-language-server",
-				"bash-language-server",
-				"shellcheck",
-				"shfmt",
-				"tfsec",
-				"prettier",
-			},
-		},
-		{
-			"nvimtools/none-ls.nvim",
-			opts = function(_, opts)
-				local nls = require("null-ls")
-				opts.sources = vim.list_extend(opts.sources, {
-					nls.builtins.formatting.shfmt.with({
-						extra_args = { "-i", "2", "-ci", "-kp" },
-					}),
-				})
-			end,
-		},
-		{ import = "plugins" },
-	},
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "awk-language-server",
+        "bash-language-server",
+        "shellcheck",
+        "shfmt",
+        "tfsec",
+        "prettier",
+      },
+    },
+    {
+      "nvimtools/none-ls.nvim",
+      opts = function(_, opts)
+        local nls = require("null-ls")
+        opts.sources = vim.list_extend(opts.sources, {
+          nls.builtins.formatting.shfmt.with({
+            extra_args = { "-i", "2", "-ci", "-kp" },
+          }),
+        })
+      end,
+    },
+    { import = "plugins" },
+  },
 }
