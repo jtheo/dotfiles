3fed053058068e135c2e9b4f036216a58ef9c3a1
Matteo Marchelli <matteo.marchelli@gmail.com>
2025-05-01 14:06:26 +0100

    added masonUpdateAll to nvim

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -13,17 +13,10 @@ return {
 				"zls",
 			},
 		},
-		{
-			"nvimtools/none-ls.nvim",
-			opts = function(_, opts)
-				local nls = require("null-ls")
-				opts.sources = vim.list_extend(opts.sources, {
-					nls.builtins.formatting.shfmt.with({
-						extra_args = { "-i", "2", "-ci", "-kp" },
-					}),
-				})
-			end,
-		},
 		{ import = "plugins" },
+	},
+	{
+		"RubixDev/mason-update-all",
+		config = true,
 	},
 }
