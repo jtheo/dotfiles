0da064d01255d00ede57fd5bd4c9ca30964be8e4
jtheo <matteo.marchelli@gmai.com>
2025-08-30 13:52:17 +0100

    temp fix to catpuccin breaking change - https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3202799735

--- private_dot_config/nvim/lua/plugins/theme.lua
+++ private_dot_config/nvim/lua/plugins/theme.lua
@@ -1,14 +1,23 @@
 return {
-	{
-		"catppuccin",
-		opts = {
-			transparent_background = true,
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
-	},
+  {
+    "catppuccin",
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }
