40dfb3913293edcbefa0914558c6edddfec2dff1
Matteo Marchelli <matteo.marchelli@gmail.com>
2025-11-14 09:31:54 +0000

    auto fmt for hcl and disable inlay hints in neovim, WIP for remove k8s node

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -4,3 +4,16 @@
 
 vim.cmd([[autocmd BufRead,BufNewFile *.Jenkinsfile setfiletype groovy]])
 vim.cmd([[autocmd BufRead,BufNewFile Jenkinsfile setfiletype groovy]])
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*.hcl",
+	callback = function()
+		vim.notify("Formatting with terragrunt hclfmt...")
+		local file = vim.fn.expand("%:p")
+		vim.fn.jobstart("terragrunt hclfmt --terragrunt-hclfmt-file " .. vim.fn.shellescape(file), {
+			on_exit = function()
+				vim.cmd("e!")
+			end,
+		})
+	end,
+})
--- private_dot_config/nvim/lua/plugins/yaml.lua
+++ private_dot_config/nvim/lua/plugins/yaml.lua
@@ -1,26 +1,19 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      yamlls = {
-        settings = {
-          -- redhat = { telemetry = { enabled = false } },
-          yaml = {
-            keyOrdering = false,
-            format = {
-              enable = false,
-            },
-            -- validate = true,
-            -- schemaStore = {
-            --   -- Must disable built-in schemaStore support to use
-            --   -- schemas from SchemaStore.nvim plugin
-            --   enable = false,
-            --   -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-            --   url = "",
-            -- },
-          },
-        },
-      },
-    },
-  },
+	"neovim/nvim-lspconfig",
+	opts = {
+		inlay_hints = { enabled = false },
+		servers = {
+			yamlls = {
+				settings = {
+					-- redhat = { telemetry = { enabled = false } },
+					yaml = {
+						keyOrdering = false,
+						format = {
+							enable = false,
+						},
+					},
+				},
+			},
+		},
+	},
 }
